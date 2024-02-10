aeda71d9163d6ed5a582d184b1d8040a70bb3c00
Aniebiet Francis <francisaniebiet678@gmail.com>
2024-02-10 16:54:21 +0100

    updates

--- src/auth/security/login.js
+++ src/auth/security/login.js
@@ -46,7 +46,7 @@ export const Login = () => {
                                     </div>
                                     <div className='pass'>
                                         <label>Password</label>
-                                        <input name='pass' type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} placeholder='********'></input>
+                                        <input name='pass' type={showPassword ? 'text' : 'password'} value={password} onChange={(e) => setPassword(e.target.value)} placeholder={showPassword ? 'Enter your password' : '********'}></input>
                                         <div className='togglebtn'>
                                             <button type='button' onClick={togglePass}>{showPassword ? <FaEye /> : <FaEyeSlash /> }</button>
                                         </div>
