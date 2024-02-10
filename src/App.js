95a4a28e849bf2c1fa9076c2c279ed9c83b15900
Aniebiet Francis <francisaniebiet678@gmail.com>
2024-02-10 17:47:58 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Routes, Route} from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom'
 import { Login } from './auth/security/login';
 import { ResetPass } from './auth/update/forgetPass'
 
