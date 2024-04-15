90764f92bb7ebf300df0ae275152660c5052474c
Aniebiet Francis <francisaniebiet678@gmail.com>
2024-04-15 11:45:24 +0100

    updates

--- src/auth/verify/resetPassVerify.js
+++ src/auth/verify/resetPassVerify.js
@@ -9,20 +9,26 @@ export const ResetVerification = () => {
     useEffect(() => {
         const verifyRoute = async () => {
             try {
-                const res = await axios.get(`https://pedxo-backend.onrender.com/auth/verify-reset-password-otp?${encodedCode}`);
+                await axios.get(`https://pedxo.netlify.app/resetver/?${encodedCode}`);
+            } catch(err) {
+                console.log('err', err);
+            }
+
+            try {
+                const res = await axios.post('https://pedxo-backend.onrender.com/auth/verify-reset-password-otp', encodedCode);
+
                 if(res) {
                     console.log(res)
-                    localStorage.setItem('t', encodedCode)
                     window.location.href = 'https://pedxo.netlify.app/reset-password/update';
                 }
             } catch(err) {
-                console.log('err', err);
+                console.log(err)
             }
 
         };
 
         verifyRoute();
-    }, [encodedCode])
+    }, [encodedCode]);
 
     return (
         <div></div>
