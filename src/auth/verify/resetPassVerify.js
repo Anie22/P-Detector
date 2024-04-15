6241536907f20f486715dadf59f5191dfbf32bf8
Aniebiet Francis <francisaniebiet678@gmail.com>
2024-04-15 12:44:56 +0100

    updates

--- src/auth/verify/resetPassVerify.js
+++ src/auth/verify/resetPassVerify.js
@@ -1,36 +1,28 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
-
+import { useEffect } from "react";
 
 export const ResetVerification = () => {
-    const [encodedCode] = useParams();
+    const encodedCode = new URLSearchParams(window.location.search)
+
+    const code = encodedCode.get('encodedCode');
+
+    const endCode = `"${code}"`
 
     useEffect(() => {
         const verifyRoute = async () => {
             try {
-                await axios.get(`https://pedxo.netlify.app/resetver/?${encodedCode}`);
-            } catch(err) {
-                console.log('err', err);
-            }
-
-            try {
-                const res = await axios.post('https://pedxo-backend.onrender.com/auth/verify-reset-password-otp', encodedCode);
-
-                if(res) {
-                    console.log(res)
-                    window.location.href = 'https://pedxo.netlify.app/reset-password/update';
+                const res = await axios.post('https://pedxo-backend.onrender.com/auth/verify-reset-password-otp', endCode)
+                if(res){
+                    window.location.href = 'https://pedxo.netlify.app/reset-password/update'
                 }
             } catch(err) {
-                console.log(err)
+                if(err) {
+                    window.location.href = 'https://pedxo.netlify.app/reset-password/verify'
+                }
             }
 
         };
 
         verifyRoute();
-    }, [encodedCode]);
-
-    return (
-        <div></div>
-    )
+    }, [code]);
 }
