aed2be3beef464fcd99fe244fcaaac97c05ff5a8
Anie22 <francisaniebiet678@gmail.com>
2024-08-10 09:18:00 +0100

    updates

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = 'https://p-detector-backend-api.onrender.com';
-const BASE_URL = 'http://127.0.0.1:8000/'
+const BASE_URL = 'https://p-detector-backend-api.onrender.com';
+// const BASE_URL = 'http://127.0.0.1:8000/'
 const accessToken = localStorage.getItem('token')
 
 export default axios.create({
